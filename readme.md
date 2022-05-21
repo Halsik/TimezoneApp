68eed707089b9c9620664764185f83c454a82ea1
Michal Hals <michal.jan.hals@gmail.com>
2022-05-21 14:22:41 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+## TimezoneApp / Chrome extension
+
+Timezone App created with http://worldtimeapi.org/ API
+
+How to use as extension:
+
+1. Clone this repo
+
+2. Go to: chrome://extensions/
+
+3. Turn on Developer Mode
+
+4. Click Load Unpacked
+
+5. Select cloned repo
+
+Now you should be able to open this app as the extension (look next to the search bar)
+
+Choose timezone and check what time is it and all regions! 🕑 🕑 🕑
+
+Link to the site :)
+
+https://halsik.github.io/TimezoneApp/
+
---
+++ script.js
@@ -0,0 +1,94 @@
+
+// Time Buttons
+const europeButton = document.querySelector(".europe");
+const asiaButton = document.querySelector(".asia");
+const americaButton = document.querySelector(".america");
+const australiaButton = document.querySelector(".australia");
+const africaButton = document.querySelector(".africa");
+
+// Timer
+const clock = document.querySelector(".time");
+
+
+// Acces to API 
+let time = {
+    
+    // Fetch data
+    fetchTime: function(region, area) {
+        fetch(
+            "http://worldtimeapi.org/api/timezone/" + region + "/" + area
+            ).then ((response) => response.json())
+            .then((data) => this.displayTime(data));
+    },
+
+    // Display data from API
+    displayTime: function(data) {
+        // Get time data from JSON
+        const { datetime } = data;
+        // Remove characters from start and end of the datatime data
+        let str = datetime
+        let firstCut = str.substr(11, str.length);
+        let newStr = firstCut
+        newStr = firstCut.substr(0, newStr.length-13)
+        // Print data in HTML
+        clock.innerHTML = newStr
+        // Change background image every button click
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + region + "')"
+    },
+    
+}
+
+// Deafult time
+time.fetchTime("europe", "warsaw")
+
+
+// Remove class "pressed"
+function resetButton() {
+    europeButton.classList.remove("pressed");
+    asiaButton.classList.remove("pressed");
+    americaButton.classList.remove("pressed");
+    australiaButton.classList.remove("pressed");
+    africaButton.classList.remove("pressed");
+}
+
+// Button event listner and add "pressed" class
+europeButton.addEventListener('click', function() {
+    resetButton()
+    europeButton.classList.toggle("pressed")
+    region = "europe"
+    area = "warsaw"
+    time.fetchTime(region, area)
+});
+
+asiaButton.addEventListener('click', function() {
+    resetButton()
+    asiaButton.classList.toggle("pressed")
+    region = "asia"
+    area = "tokyo"
+    time.fetchTime(region, area)
+});
+
+americaButton.addEventListener('click', function() {
+    resetButton()
+    americaButton.classList.toggle("pressed")
+    region = "america"
+    area = "new_york"
+    time.fetchTime(region, area)
+});
+
+africaButton.addEventListener('click', function() {
+    resetButton()
+    africaButton.classList.toggle("pressed")
+    region = "africa"
+    area = "cairo"
+    time.fetchTime(region, area)
+});
+
+australiaButton.addEventListener('click', function() {
+    resetButton()
+    australiaButton.classList.toggle("pressed")
+    region = "australia"
+    area = "sydney"
+    time.fetchTime(region, area)
+});
+
